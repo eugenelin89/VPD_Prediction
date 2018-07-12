a9b00af5e2c97e60b017f6c55db39a2cbc659475
Eugene Lin <eugenelin89@gmail.com>
2018-07-12 13:31:06 -0700

    updating UI

--- ui.R
+++ ui.R
@@ -4,8 +4,21 @@ library(leaflet)
 
 shinyUI(fluidPage(
   titlePanel("What crime will you experinece in City of Vancouver"),
+  
+  tags$ul(
+    tags$li("Click on the map somewhere in the City of Vancouver, specify a date and a time, the app will predict what crime you are likely to encounter at the specified location at the time of the day."), 
+    tags$li("The map also displays location of all reported crimes in the City of Vancouver in 2017. Click on the circle on the map to explore."), 
+    tags$li("The app builds on data provided by the Vancouver Police Department thru ",tags$a(href="https://data.vancouver.ca/datacatalogue/crime-data.htm", "City of Vancouver's Open Data portal"),"."),
+    tags$li("We partitioned the data into 75% training and 25% validation set. Training is peformed on the training set using Random Forest, with k-fold cross-validation where k=5."),
+    tags$li("Due to privacy concern, certain types of crime are not included in the data."),
+    tags$li("Source code can be found on",tags$a(href="https://github.com/eugenelin89/VPD_Prediction", "Github"),".")
+  ),
+  
   sidebarLayout(
     sidebarPanel(
+      
+
+      
       dateInput("date_input", "Enter Date", value = NULL, min = NULL, max = NULL,
                 format = "yyyy-mm-dd", startview = "month", weekstart = 0,
                 language = "en", width = NULL, autoclose = TRUE),
@@ -13,8 +26,8 @@ shinyUI(fluidPage(
     ),
     mainPanel(
 
-      
-      
+      h4("Map may take a moment to load. Thank you for your patience!"),
+      p(),
       leafletOutput("mymap"),
       p(),
       h4(textOutput("lat")),
